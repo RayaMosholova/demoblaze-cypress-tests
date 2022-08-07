8d70f52bced5dfb3d80c7f035ba14eba639de7a8
Raya Mosholova <raya.mosholova@gmail.com>
2022-08-07 23:13:19 +0300

    Initial implementation shopping feature

--- cypress/e2e/shopping/shopping.js
+++ cypress/e2e/shopping/shopping.js
@@ -1,5 +1,140 @@
-import {Given, When, Then, And } from '@badeball/cypress-cucumber-preprocessor'
+import { Given, When, Then, And } from '@badeball/cypress-cucumber-preprocessor'
+import { defaultUser } from '../../support/factories/user-factory'
+import products from '../fixtures/products.json'
 
 Given('the user in authorized', () => {
-    return 'pending';
+    const { username, password } = defaultUser
+    const encodedPassword = btoa(password)
+    cy.request('POST', 'https://api.demoblaze.com/login', {
+        username,
+        password: encodedPassword
+    }).as('login')
+
+    cy.get('@login').then(response => {
+        const body = response.body
+        expect(body).to.contain("Auth_token")
+    })
+})
+
+And('the cart is emptied', () => {
+    cy.get('@login').then(response => {
+        const authToken = response.body.split(': ')[1]
+        cy.request('POST', 'https://api.demoblaze.com/viewcart', {
+            cookie: authToken,
+            flag: true
+        }).then(response => {
+            response.body['Items'].forEach(item => {
+                cy.request('POST', 'https://api.demoblaze.com/deleteitem', {
+                    id: item.id
+                })
+            })
+        })
+    })
+})
+
+When('the user adds "samsung galaxy s6" and "htc one m9" to the cart', () => {
+    cy.get('@login').then(response => {
+        const authToken = response.body.split(': ')[1]
+        cy.request('POST', 'https://api.demoblaze.com/addtocart', {
+            id: products.galaxyS6.id,
+            cookie: authToken,
+            prod_id: products.galaxyS6.prod_id,
+            flag: true
+        }).then(response => {
+            expect(response.status).to.eq(200)
+        })
+
+        cy.request('POST', 'https://api.demoblaze.com/addtocart', {
+            id: products.htc.id,
+            cookie: authToken,
+            prod_id: products.htc.prod_id,
+            flag: true
+        }).then(response => {
+            expect(response.status).to.eq(200)
+        })
+    })
+})
+
+Then('these items are placed in the cart', () => {
+    cy.get('@login').then(response => {
+        const authToken = response.body.split(': ')[1]
+        cy.request('POST', 'https://api.demoblaze.com/viewcart', {
+            cookie: authToken,
+            flag: true
+        }).then(response => {
+            [products.galaxyS6.id, products.htc.id].forEach(id => {
+                const found = response.body['Items'].find(product => product.id === id)
+                expect(found).not.to.be.undefined
+            })
+        })
+    })
+})
+
+When('the user adds few items to the cart', () => {
+    cy.get('@login').then(response => {
+        const authToken = response.body.split(': ')[1]
+        cy.request('POST', 'https://api.demoblaze.com/addtocart', {
+            id: products.galaxyS6.id,
+            cookie: authToken,
+            prod_id: products.galaxyS6.prod_id,
+            flag: true
+        }).then(response => {
+            expect(response.status).to.eq(200)
+        })
+
+        cy.request('POST', 'https://api.demoblaze.com/addtocart', {
+            id: products.htc.id,
+            cookie: authToken,
+            prod_id: products.htc.prod_id,
+            flag: true
+        }).then(response => {
+            expect(response.status).to.eq(200)
+        })
+    })
+
+})
+
+Then('the cart is not empty', () => {
+    cy.get('@login').then(response => {
+        const authToken = response.body.split(': ')[1]
+        cy.request('POST', 'https://api.demoblaze.com/viewcart', {
+            cookie: authToken,
+            flag: true
+        }).then(response => {
+            [products.galaxyS6.id, products.htc.id].forEach(id => {
+                const found = response.body['Items'].find(product => product.id === id)
+                expect(found).not.to.be.undefined
+            })
+        })
+    })
+
+})
+
+When('the user deletes all items from the cart', () => {
+    cy.get('@login').then(response => {
+        const authToken = response.body.split(': ')[1]
+        cy.request('POST', 'https://api.demoblaze.com/viewcart', {
+            cookie: authToken,
+            flag: true
+        }).then(response => {
+            response.body['Items'].forEach(item => {
+                cy.request('POST', 'https://api.demoblaze.com/deleteitem', {
+                    id: item.id
+                })
+            })
+        })
+    })
+
+})
+
+Then('the cart is empty', () => {
+    cy.get('@login').then(response => {
+        const authToken = response.body.split(': ')[1]
+        cy.request('POST', 'https://api.demoblaze.com/viewcart', {
+            cookie: authToken,
+            flag: true
+        }).then(response => {
+            expect(response.body['Items'].length).to.eq(0)
+        })
+    })
 })
