5d24524a32c0e3ddbc2e3f2675df203715da1119
Raya Mosholova <raya.mosholova@gmail.com>
2022-08-07 21:08:19 +0300

    Initial implementation entries-endpoint feature

--- cypress/e2e/entries-endpoint.feature
+++ cypress/e2e/entries-endpoint.feature
@@ -1,10 +1,11 @@
 Feature: Entries endpoint
 
-  Scenario: '/entries' endpoint returns 9 results
+  Background:
     Given the user sends get request to the endpoint
+
+  Scenario: '/entries' endpoint returns 9 results
     Then the response returns 9 results
 
   Scenario: Items from '/entries' endpoint have correct price
-    Given the user sends get request to the endpoint
-    Then the response returns 2 items with price less than 400 and 7 items with price more than 400 
+    Then the response returns 2 items with price less than 400 and 7 items with price more than 400
   
--- cypress/e2e/entries-endpoint/entries-endpoint.js
+++ cypress/e2e/entries-endpoint/entries-endpoint.js
@@ -1,5 +1,27 @@
-import {Given, When, Then, And } from '@badeball/cypress-cucumber-preprocessor'
+import { Given, When, Then, And } from '@badeball/cypress-cucumber-preprocessor'
 
 Given('the user sends get request to the endpoint', () => {
-    return 'pending';
+    cy.request('https://api.demoblaze.com/entries').as('getItems')
+})
+
+Then('the response returns 9 results', () => {
+    cy.get('@getItems').then(response => {
+        expect(response.body['Items']).to.have.length(9)
+    })
+})
+
+Then('the response returns 2 items with price less than 400 and 7 items with price more than 400', () => {
+    cy.get('@getItems').then(response => {
+        let itemsOver400 = 0
+        let itemsBelow400 = 0
+        response.body['Items'].forEach(item => {
+            if (item.price > 400) {
+                itemsOver400 += 1
+            } else {
+                itemsBelow400 += 1
+            }
+        })
+        expect(itemsBelow400).to.be.equal(2)
+        expect(itemsOver400).to.be.equal(7)
+    })
 })
